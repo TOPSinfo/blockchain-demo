9c10cfeed9b4e0a4ec7d7cf08aa8f00c44e7de7c
narendra <narendra.chauhan@topsinfosolutions.com>
2019-06-06 14:42:16 +0530

    initial redux setup completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-import Home from "./components/Home/Home"
+import  History  from './history';
+import { Router,Route,Switch,Redirect } from 'react-router-dom'
+import Home from "./components/Home/Home";
+
 import "./App.css";
 
 class App extends Component {
@@ -9,13 +12,9 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
@@ -23,11 +22,8 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -37,14 +33,9 @@ class App extends Component {
 
   runExample = async () => {
     const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
     await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
     const response = await contract.methods.get().call();
 
-    // Update state with the result.
     this.setState({ storageValue: response });
   };
 
