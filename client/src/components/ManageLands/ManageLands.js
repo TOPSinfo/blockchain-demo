3ab4facbb9588d728650a7097a244d90cf14fa24
Arjun <arjun@topsinfosolutions.com>
2019-06-11 18:29:42 +0530

    worked on forntend of lands

--- client/src/components/ManageLands/ManageLands.js
+++ client/src/components/ManageLands/ManageLands.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import Header from "../Header/Header";
 import { MDBDataTable } from 'mdbreact';
 
-import {Modal, Table, Alert} from "react-bootstrap";
+import {Modal, Table, Alert, Row, Col} from "react-bootstrap";
 import { connect } from 'react-redux';
 import {withRouter} from 'react-router-dom'
 import {landManagment}  from "../../actions"
 import config from '../../config/accounts';
+import ReactJson from 'react-json-view'
+
 const Tx = require('ethereumjs-tx');
 class ManageLands extends Component {
 
@@ -14,7 +16,18 @@ class ManageLands extends Component {
         super(props);
         this.state = {
           showAddLandsModal:false,
-          submitted: false
+          submitted: false,
+          showDetailModal:false,
+          myJsonObj1:{
+              blockHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+              blockNumber: null,
+              from:"0x72019284f6eF69f9a322193c91185222e80327B4",
+              gas:8000000,
+              gasPrice:"10000000000",
+              hash:"0xc2252d5c82609f4cbf5223aa61ba643697cfda53bbb7e810a3179e79acabb734",
+              input:"0xa0d09221000000000000000000000000000000000000000000000000000000000000004000000000000000000000000072019284f6ef69f9a322193c91185222e80327b400000000000000000000000000000000000000000000000000000000000000077364666473616600000000000000000000000000000000000000000000000000",
+              nonce:12
+          }
         }
     }
 
@@ -74,28 +87,11 @@ class ManageLands extends Component {
 
             this.toogleAddLandModal()
       }
+    }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
-
-
+    toogleDetailModal = () =>{
+      this.setState({showDetailModal:!this.state.showDetailModal});
     }
     render() {
       const { landName, submitted} = this.state;
@@ -128,26 +124,26 @@ class ManageLands extends Component {
                     <p className="text-white">Here you can Create new Lands, and Manage the Lands owned by you.</p>
                     <a onClick={this.toogleAddLandModal} className="primary-btn header-btn text-uppercase mb-20">Add Land</a>
                 </div>
-            {/* <Table className="white-color" striped bordered hover>
+           <Table className="white-color" striped bordered hover>
             <thead>
               <tr>
                 <th >#</th>
-                <th>Username</th>
-                <th>Address</th>
+                <th>Landname</th>
+                <th>Current Owner</th>
+                <th>Created At</th>
+                <th>View Details</th>
               </tr>
             </thead>
             <tbody>
-              {/* {this.state.userData.map((user,index)=>{
-                if(index !== 0){
-                  return(<tr key={index}>
-                    <td>{index}</td>
-                    <td>{user.username}</td>
-                    <td>{user.address}</td>
-                  </tr>)
-                }
-              })} */}
-            {/* </tbody>
-          </Table> */}
+              <tr>
+                <td>1.</td>
+                <td>Vastrapur</td>
+                <td>Arjun</td>
+                <td>11th August,2019</td>
+                <td><button onClick={this.toogleDetailModal}>View Details</button></td>
+              </tr>
+           </tbody>
+          </Table>
             </div>
         </div>
     </section>
@@ -169,6 +165,44 @@ class ManageLands extends Component {
               <a onClick={this.handleAddLand} className="primary-btn header-btn text-uppercase mb-20 login-button">Submit</a>
             </div>
 
+          </Modal.Body>
+        </Modal>
+      <Modal size="lg" show={this.state.showDetailModal} onHide={this.toogleDetailModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Land Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Row className="show-grid">
+              <Col xs={12} md={12}>
+              <Table  striped bordered hover>
+            <thead>
+              <tr>
+                <th >#</th>
+                <th>Landname</th>
+                <th>Current Owner</th>
+                <th>Created At</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1.</td>
+                <td>Vastrapur</td>
+                <td>Arjun</td>
+                <td>11th August,2019</td>
+              </tr>
+           </tbody>
+          </Table>
+              </Col>
+              <Col xs={6} md={4}>
+              <ReactJson src={this.state.myJsonObj1} />
+              </Col>
+            </Row>
+            <div className="col-lg-6 cols">
+
+            </div>
+            <div className="col-lg-6 cols">
+
+            </div>
           </Modal.Body>
         </Modal>
 
