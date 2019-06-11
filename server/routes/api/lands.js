dffd421f03768a30f835495afc5c66932ad5e37e
narendra <narendra.chauhan@topsinfosolutions.com>
2019-06-11 19:05:53 +0530

    api for land is done

--- server/routes/api/lands.js
+++ server/routes/api/lands.js
@@ -1,11 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport");
-
-
 const Land = require("../../models/Land");
 
 router.post("/createland", (req, res) => {
@@ -23,5 +17,39 @@ router.post("/createland", (req, res) => {
     .catch(err => console.log(err));
 });
       
+
+router.post("/getland", (req, res) => {
+    Land.find({ _id : req.body.id,currentOwner:req.body.currentOwner }).then(land => {
+       if (land) {
+        return res.status(200).json({ land:land });
+      } else {
+        return res.status(400).json({ response:"No land found" });
+      }
+    });
+});
+
+
+router.get("/getallland", (req, res) => {
+    
+    Land.find().then(land => {
+       if (land) {
+        return res.status(200).json({ land:land });
+      } else {
+        return res.status(400).json({ response:"No land found" });
+      }
+    });
+});
+
+
+router.post("/get-land-by-owner", (req, res) => {
+    Land.find({currentOwner:req.body.currentOwner}).then(land => {
+       if (land) {
+        return res.status(200).json({ land:land });
+      } else {
+        return res.status(400).json({ response:"No land found" });
+      }
+    });
+});
+
 
 module.exports = router;
