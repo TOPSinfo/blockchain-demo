6ffac792beb62042deb6eaf412e41d3540252847
narendra <narendra.chauhan@topsinfosolutions.com>
2019-06-11 17:09:03 +0530

    create land api done

---
+++ server/models/Land.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const LandSchema = new Schema({
+    landName:{
+        type: String,
+        required: true },
+    currentOwner :{
+        type: String,
+        required: true },
+    history : [
+        {
+            OwnerAddress: { type: String, default:null}, 
+            username: { type: String,default:null }, 
+            Timestamp:{ type: Date, default: Date.now }
+        }
+    ]
+});
+
+module.exports = Land = mongoose.model("lands", LandSchema);
